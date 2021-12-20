1d4c060661e8e68bc12c1f2880cec0679e8b7a64
Glenn Sandvoss <gsandvoss@gmail.com>
2021-12-19 18:23:38 -0800

    adding files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :pins
+  devise_for :users
   root 'home#index'
   get  'home/about'
-  
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
