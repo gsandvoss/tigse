93de64aaac18baa7035c61983d5c9e9b33c0fee6
Glenn Sandvoss <gsandvoss@gmail.com>
2021-12-30 11:52:38 -0800

    trying to get this to compile!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,12 +5,12 @@
 
 
 
-//import "bootstrap";
+import "bootstrap"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-//import "bootstrap/dist/css/bootstrap.min.css";
+
 
 
 Rails.start()
